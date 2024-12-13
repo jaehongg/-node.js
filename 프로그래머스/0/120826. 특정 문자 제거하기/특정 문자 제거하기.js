648e1bd566e474091f9531243829be63eae6dfb2
서재홍 <107832252+jaehongg@users.noreply.github.com>
2024-12-13 16:42:37 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 2.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,5 +1,6 @@
 function solution(my_string, letter) {
     const answer = my_string.replace(new RegExp(letter, 'g'), '');
+    console.log(my_string.split(letter).join(''))
     
     return answer;
 }
